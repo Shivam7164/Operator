bd7f103e36d051cf0be747516b333218f06af623
shivam <shivamrajpoot3703@gmail.com>
2022-08-12 23:39:29 +0530

    added

---
+++ Blog2/script.js
@@ -0,0 +1,141 @@
+// 11 % 3 // output
+2
+
+
+// 1 + "JS"
+'1JS'
+
+// 1 * "JS"
+NaN
+
+// 5 + true
+6
+
+
+// 6 - true
+5
+
+// 7 - false
+7
+
+// 1 + 2 * 3 / 4
+2.5
+
+// 8 + false
+8
+
+// true + true
+2
+
+// 7 ** 2
+49
+
+// 2 ** 3
+8
+
+// "hello" + " " + "world"
+"hello world"
+
+// "hello" * 10
+NaN
+
+// true + false
+1
+
+// true + false
+1
+
+// true + false
+1
+
+// +true
+1
+
+// +"21"
+21
+
+// +null
+0
+
+// Keeping the code below in mind write the output next to the line of code:
+
+// let count = 46;
+// count++ // output
+count
+47
+// ++count
+48
+
+// count++
+count
+49
+
+// ++count
+50
+
+// count++ + 10
+60
+
+// ++count + 5
+57
+
+// count++
+count
+52
+
+// count += 10
+count += 10
+63
+
+// count *= 5
+351
+
+// Write the output of each line of code keeping in mind the operator precedence:
+
+// 3 + 4 * 5 // output
+23
+
+// (25 + 28) + 105
+158
+
+// 4 * 3 ** 2
+36
+
+// 4, 400 + 10
+410
+
+// (4, 400) + 10
+410
+
+// 3 + "10" * 2
+23
+
+// 3 + 10 * "2"
+23
+
+// "3" + 10 * 2
+'320'
+
+// 3 + 10 * 2
+23
+
+// 3 + (10 * 2)
+23
+
+// (3 + 10) * 2
+26
+
+// 2 ** 3 ** 2
+512
+
+// 2 ** (3 ** 2)
+512
+
+// (2 ** 3) ** 2
+64
+
+// +null + 10
+10
+
+// +"21" + (10 ,20)
+41
